122bfdc72df5746ef0be08ba42d3a436f4d6ae32
wnsghd14 <wnsghd14@gmail.com>
2022-11-10 11:33:55 +0900

    feat: base.html 네브바 구현

--- pjt/urls.py
+++ pjt/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 urlpatterns = [
+    path('',views.base,name="base"),
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
