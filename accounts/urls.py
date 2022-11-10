047e3c046a31e40ffe5850c34934811ef99be545
jn-97 <kjo5395@gmail.com>
2022-11-10 11:08:18 +0900

    fead:accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("<int:pk>/", views.profile, name="profile"),
+    path("<int:pk>/update/", views.update, name="update"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
 ]
