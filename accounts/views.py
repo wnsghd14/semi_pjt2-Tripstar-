07fc018c88a021ab41d69a1f7bce43ea779213db
dongwonbaek <bmrio100@gmail.com>
2022-11-14 23:53:41 +0900

    fix: 회원가입 후 로그인불가현상 해결

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def signup(request):
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
-            auth_login(request, user)
+            auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
             return redirect("articles:index")
     else:
         form = CustomUserCreationForm()
