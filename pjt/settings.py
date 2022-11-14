0f376026b5b412af3d01189b718e5721cee759f7
wnsghd14 <wnsghd14@gmail.com>
2022-11-14 11:46:19 +0900

    chore: 미디어파일 gitignore

--- pjt/settings.py
+++ pjt/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-MEDIA_ROOT = BASE_DIR / "images"
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
