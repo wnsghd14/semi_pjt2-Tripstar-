1b5b5ee6e33d79e8c2d036968dc2161d96236e89
jhkim9028 <jhkim9028@gmail.com>
2022-11-16 17:45:31 +0900

    feat: 예약, 결제 연결

---
+++ cart/forms.py
@@ -0,0 +1,6 @@
+# from .models import Pay
+# from django import forms
+# class PayForm(forms.ModelForm):
+#     class Meta:
+#         model = Pay
+#         fields = ('__all__')
--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,11 @@
-from django.db import models
-from articles.models import Reservation, Article
-from accounts.models import User
-# Create your models here.
+# from django.db import models
+# from articles.models import Reservation, Article
+# from accounts.models import User
+# # Create your models here.
 
-class Pay(models.Model):
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+# class Pay(models.Model):
+#     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
-# Create your models here.
+
