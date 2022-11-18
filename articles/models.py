38e46607793208810812c77d5db4404d9c2bd62a
dongwonbaek <bmrio100@gmail.com>
2022-11-18 15:20:27 +0900

    chore: 코멘트 작성시간 추가

--- articles/models.py
+++ articles/models.py
@@ -23,7 +23,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = ProcessedImageField(upload_to='images/articles/',null=True,
-                                processors=[ResizeToFill(400, 300)],
+                                processors=[ResizeToFill(800, 600)],
                                 format='JPEG',
                                 options={'quality': 80})
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -76,6 +76,7 @@ class Comment(models.Model):
     content = models.TextField()
     review = models.ForeignKey(Review,on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Age(models.IntegerChoices):
     zero = 0, '0'
