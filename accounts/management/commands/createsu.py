91539dbf0ed0b243aa02e55e8bd267058783e8ad
wnsghd14 <wnsghd14@gmail.com>
2022-11-20 23:06:09 +0900

    되라되라

--- accounts/management/commands/createsu.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
-
-class Command(BaseCommand):
-
-    def handle(self,*args,**options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin","admin@admin.com","admin")
-            self.stdout.write(self.style.SUCCESS('Successful created new super user'))
